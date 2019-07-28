93e1dfdda1960e14ad75e8a3998426aa0847bbf5
okrad <enrico.triolo@gmail.com>
2019-07-28 17:00:37 +0200

    Rollup config cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,8 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
-const production = !process.env.ROLLUP_WATCH;
-
-const name = pkg.name
-	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
-	.replace(/^\w/, m => m.toUpperCase())
-	.replace(/-\w/g, m => m[1].toUpperCase());
+const name = 'ProgressBar';
 
 export default {
 	input: 'src/index.svelte',
@@ -19,35 +12,12 @@ export default {
 	],
 	plugins: [
 		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
 			css: css => {
 				css.write('index.css');
 			}
 		}),
-
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration —
-		// consult the documentation for details:
-		// https://github.com/rollup/rollup-plugin-commonjs
-		resolve({
-			browser: true,
-			dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
-		}),
-//		commonjs(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
+		resolve()
 	],
-	watch: {
-		clearScreen: false
-	}
 };
