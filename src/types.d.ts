184345e3a754d539fffc630d326b9ce6078ceb47
okrad <enrico.triolo@gmail.com>
2020-10-26 23:56:12 +0100

    Better TS compliance

---
+++ src/types.d.ts
@@ -0,0 +1,50 @@
+declare module "@okrad/svelte-progressbar" {
+
+	export default class ProgressBar {
+
+		$$prop_def: {
+			series: any,
+			style ?: string,
+			showProgressValue ?: boolean,
+			width ?: number,
+			thickness ?: number,
+			height ?: number,
+			textSize ?: number,
+			stackSeries ?: boolean,
+			margin ?: number,
+			addBackground ?: boolean,
+			bgFillColor ?: string,
+			bgColor ?: string,
+			labelColor ?: string,
+			valueLabel ?: string,
+			cls ?: string,
+			rx ?: number,
+			ry ?: number,
+			path ?: string,
+			colors ?: Array<string>,
+			thresholds ?: Array<Threshold>
+		}
+
+	};
+
+}
+
+export interface SeriesStore {
+	subscribe,
+	set,
+	series ?: Array<Series>,
+	updateSeries: (newSeries: Array<Series>) => void,
+	updateLabel: (newLabel: string) => void
+}
+
+export interface Series {
+	perc: number,
+	color ?: string,
+	prevOffset ?: number,
+	radius ?: number
+}
+
+export interface Threshold {
+	till: number,
+	color: string
+}
